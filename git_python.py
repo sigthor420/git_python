5af21f9b61f17f2d54ebd592ff178126f7742daf
sigthor420 <sigthorge244@nemi.tskoli.is>
2017-01-25 10:58:54 +0000

    liður 1 fix

--- git_python.py
+++ git_python.py
@@ -2,6 +2,8 @@
 #biður um tölur
 tala1 = float(input("Sláðu inn tölu: "))
 tala2 = float(input("Sláðu inn tölu: "))
+#prentar út tala1 + tala2
+print(tala1, "+", tala2, "=", str(tala1 + tala2))
 #prentar út tala1 * tala2
 print(tala1, "*", tala2, "=", str(tala1 * tala2))
 
