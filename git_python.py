40c9b8780353b2b97cdcd5acdade2e142df56bfe
sigthor420 <sigthorge244@nemi.tskoli.is>
2017-01-25 10:41:26 +0000

    Liður #2

--- git_python.py
+++ git_python.py
@@ -4,3 +4,9 @@ tala2 = float(input("Sláðu inn tölu: "))
 
 print(tala1, "*", tala2, "=", str(tala1 * tala2))
 
+#Liður 2
+
+fornafn = input("Sláðu inn fornafn: ")
+eftirnafn = input("Sláðu inn eftirnafn: ")
+print("Halló", fornafn, eftirnafn)
+
